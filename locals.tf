bc29975f9877d3bb18ca049c3bdce52bca78c596
Rubén D. Carrera del Castillo <rdcarrera@indra.es>
2023-07-31 15:54:00 +0200

    Added fancy outputs.

--- locals.tf
+++ locals.tf
@@ -6,4 +6,21 @@ locals {
     Owner       = var.aws.owner
     Terraform   = "true"
   }
+
+  output_aws = <<EOT
+AWS Information:
+        ╠ Profile: ${var.aws.profile}
+        ╠ Region: ${var.translation_regions[var.aws.region]}
+        ╚ Owner: ${var.aws.owner}
+EOT
+
+  output_vpc = <<EOT
+VPC Information:
+${join("\n", [
+  for vpc_key, vpc_value in module.vpc : (
+    "→ (${vpc_key})${vpc_value.name}:\n\t╠ ID: ${vpc_value.vpc_id}\n\t╠ Public Subnets: ${join(", ", try(vpc_value.public_subnets, []))}\n\t╠ Private Subnets: ${join(", ", try(vpc_value.private_subnets, []))}\n\t╠ Database Subnets: ${join(", ", try(vpc_value.database_subnets, []))}\n\t╚ Elasticache Subnets: ${join(", ", try(vpc_value.elasticache_subnets, []))}"
+  )
+])}
+EOT
+
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
 output "output" {
-  value = "The aws profile is: ${var.aws.profile} and the default region is ${var.translation_regions[var.aws.region]}\nThe eks network is: ${jsonencode(data.aws_subnets.eks_network["main"])}"
-  # The value of the VPC are:\n${jsonencode(module.vpc["main"])}"
+  value = "${local.output_aws}\n${local.output_vpc}"
 }
