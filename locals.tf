6f1a408aaa4d6f7ad4288bba057761b706ceb474
Rubén D. Carrera del Castillo <rdcarrera@indra.es>
2023-08-07 14:05:30 +0200

    better output.

--- locals.tf
+++ locals.tf
@@ -61,7 +61,7 @@ EOT
 VPC Information:
 ${join("\n", [
   for vpc_key, vpc_value in module.vpc : (
-    "→ (${vpc_key})${vpc_value.name}:\n\t╠ ID: ${vpc_value.vpc_id}\n\t╠ Public Subnets: ${join(", ", try(vpc_value.public_subnets, []))}\n\t╠ Private Subnets: ${join(", ", try(vpc_value.private_subnets, []))}\n\t╠ Database Subnets: ${join(", ", try(vpc_value.database_subnets, []))}\n\t╚ Elasticache Subnets: ${join(", ", try(vpc_value.elasticache_subnets, []))}"
+    "→ (${vpc_key})${vpc_value.name}:\n\t╠ ID: ${vpc_value.vpc_id}\n\t╠ Public Subnets:\n\t║\t→  ${join("\n\t║\t→  ", try(vpc_value.public_subnets, []))}\n\t╠ Private Subnets:\n\t║\t→  ${join("\n\t║\t→  ", try(vpc_value.private_subnets, []))}\n\t╠ Database Subnets:\n\t║\t→  ${join("\n\t║\t→  ", try(vpc_value.database_subnets, []))}\n\t╚ Elasticache Subnets:\n\t\t→  ${join("\n\t\t→  ", try(vpc_value.elasticache_subnets, []))}"
   )
 ])}
 EOT
